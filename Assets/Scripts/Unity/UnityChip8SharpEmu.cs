07a98f01ca9dae21bd33ce0e87bb396ba52de45f
Darryl Day <darryl@inspectar.com>
2020-10-04 20:28:25 -0230

    - Created title menu - Added preloaded ROMs - Changed how CPU cycle was set by using FixedUpdate

--- Assets/Scripts/Unity/UnityChip8SharpEmu.cs
+++ Assets/Scripts/Unity/UnityChip8SharpEmu.cs
@@ -1,6 +1,12 @@
-﻿using SimpleFileBrowser;
+﻿using Chip8Sharp.Core;
+using System;
+using System.Collections;
 using UnityEngine;
-using Chip8Sharp.Core;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+using SimpleFileBrowser;
+#endif
 
 namespace Chip8Sharp.Unity
 {
@@ -9,6 +15,10 @@ namespace Chip8Sharp.Unity
 
         [SerializeField] private Texture2DScreenRenderer _unityScreenRenderer;
         [SerializeField] private UnityBeep _unityBeep;
+
+        [SerializeField] private GameObject _titlePanel;
+        [SerializeField] private GameObject _preloadedPanel;
+        [SerializeField] private Button _selectROMButton;
 
         public bool StepThrough = false;
 
@@ -21,7 +31,6 @@ namespace Chip8Sharp.Unity
         public bool InitOnStart { get; set; } = true;
 
         private bool _gameLoaded = false;
-        private float _deltaCounter;
 
         public void Init()
         {
@@ -36,27 +45,39 @@ namespace Chip8Sharp.Unity
                 _unityRandomNumber,
                 _unityLogger
                 );
+
+            QualitySettings.vSyncCount = 0;
         }
 
         void Start()
         {
+#if UNITY_WEBGL
+            _selectROMButton.gameObject.SetActive(false);
+
+            Application.targetFrameRate = -1;
+#endif
+
             if (InitOnStart)
             {
                 Init();
-
-                FileBrowser.SetFilters(false, ".ch8");
-                FileBrowser.ShowLoadDialog((files) =>
-                {
-                    var romBytes = System.IO.File.ReadAllBytes(files[0]);
-                    CPU.LoadGame(romBytes);
-                    _gameLoaded = true;
-                },
-                () => { },
-                title: "Select Chip 8 Rom");
             }
         }
 
         void Update()
+        {
+            if (_gameLoaded)
+            {
+                if (UnityEngine.Input.GetKeyDown(KeyCode.Escape) || UnityEngine.Input.GetKeyDown(KeyCode.Backspace))
+                {
+                    CPU.Reset();
+                    _gameLoaded = false;
+
+                    _titlePanel.SetActive(true);
+                }
+            }
+        }
+
+        void FixedUpdate()
         {
             if (StepThrough)
             {
@@ -69,20 +90,73 @@ namespace Chip8Sharp.Unity
             }
             else
             {
-                _deltaCounter += Time.deltaTime;
-
-                if (_deltaCounter >= (1f / 500f))
+                if (_gameLoaded)
                 {
-                    _deltaCounter = 0f;
-
-                    if (_gameLoaded)
-                    {
-                        CPU.EmulateCycle();
-                        CPU.SetKeys();
-                    }
+                    CPU.EmulateCycle();
+                    CPU.SetKeys();
                 }
             }
-
         }
+
+        public void SelectROMFile()
+        {
+#if UNITY_EDITOR || UNITY_STANDALONE
+            FileBrowser.SetFilters(false, ".ch8");
+            FileBrowser.ShowLoadDialog((files) =>
+            {
+                try
+                {
+                    var romBytes = System.IO.File.ReadAllBytes(files[0]);
+                    LoadGame(romBytes);
+                }
+                catch (Exception e)
+                {
+                }
+            },
+            () => { },
+            title: "Select Chip 8 Rom");
+#endif
+        }
+
+        public void OpenPreloadedGame(string gameName)
+        {
+            StartCoroutine(LoadBytesFromStreamingAsset("Roms/" + gameName + ".ch8", (bytes) =>
+            {
+                if (bytes != null)
+                {
+                    LoadGame(bytes);
+                }
+            }
+            ));
+        }
+
+        private void LoadGame(byte[] bytes)
+        {
+            CPU.LoadGame(bytes);
+            _gameLoaded = true;
+
+            _titlePanel.SetActive(false);
+            _preloadedPanel.SetActive(false);
+        }
+
+        private IEnumerator LoadBytesFromStreamingAsset(string fileName, Action<byte[]> onFinished = null)
+        {
+            string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+
+            byte[] bytes;
+            if (filePath.Contains("://") || filePath.Contains(":///"))
+            {
+                var www = new WWW(filePath);
+                yield return www;
+                bytes = www.bytes;
+            }
+            else
+            {
+                bytes = System.IO.File.ReadAllBytes(filePath);
+            }
+
+            onFinished?.Invoke(bytes);
+        }
+
     }
 }
